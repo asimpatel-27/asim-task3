acb413b221cccfba7d7998b5df1ec9bca9d8b76e
Asim Nasir Patel <patelasim47@gmail.com>
2026-09-12 10:43:55 +0530

    Add files via upload

--- day1/App.jsx
+++ day1/App.jsx
@@ -1,1 +1,20 @@
+function App() {
+  const name = "Asim"
+  const course = "React Fundamentals"
 
+  return (
+    <div className="container">
+      <h1>Welcome to React</h1>
+
+      <h2>Hello, {name}!</h2>
+
+      <p>I am learning {course}.</p>
+
+      <p>This is my Day 1 React project.</p>
+
+      <button>Get Started</button>
+    </div>
+  )
+}
+
+export default App
